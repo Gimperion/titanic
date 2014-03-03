495109fe13412995d5bf150fc7eabc2a526c2970
Gimperion <gimperion@gmail.com>
2014-03-03 13:02:45 -0500

    db file import added to script

--- create_db.sh
+++ create_db.sh
@@ -6,4 +6,41 @@ psql -c "CREATE DATABASE titanic WITH ENCODING 'UTF8';"
 psql -c "GRANT ALL PRIVILEGES ON DATABASE titanic to gimperion;"
 
 ## Create db stuff here ## 
-psql -c ""
+psql -d titanic -c "CREATE TABLE titanic_test (
+    passenter_id int NOT NULL primary key,
+    passenger_class int,
+    name varchar(255),
+    gender varchar(7),
+    age float4,
+    sibsp int,
+    parch int,
+    ticket varchar(25),
+    fare float4,
+    cabin varchar(20),
+    embarked varchar(4)
+)"
+
+psql -d titanic -c "CREATE TABLE titanic_train (
+    passenter_id int NOT NULL primary key,
+    survived int,
+    passenger_class int,
+    name varchar(255),
+    gender varchar(7),
+    age float4,
+    sibsp int,
+    parch int,
+    ticket varchar(25),
+    fare float4,
+    cabin varchar(20),
+    embarked varchar(4)
+)"
+
+psql -d titanic -c "\COPY titanic_train 
+    FROM './data/train.csv' 
+    WITH DELIMITER ',' 
+    CSV HEADER;"
+
+psql -d titanic -c "\COPY titanic_test 
+    FROM './data/test.csv' 
+    WITH DELIMITER ',' 
+    CSV HEADER;"
