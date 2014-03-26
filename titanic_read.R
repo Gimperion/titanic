5fcc9b4bd8814c30a8b06e74b193ca6b8a590e15
Tommy Shen <Gimperion@gmail.com>
2014-03-25 20:35:59 -0400

    initial KNN model

---
+++ titanic_read.R
@@ -0,0 +1,88 @@
+## Processing of Titanic Data
+library(class)
+setwd("./titanic")
+source("../auto/tomkit.R")
+test <- read.csv("./data/test.csv")
+train <- read.csv("./data/train.csv")
+
+
+scale_map <- function(x,y){
+    a <- aggregate(y ~ x, data=data.frame(x,y), mean)
+    .ret <- a$y/min(a$y)
+    names(.ret) <- a$x
+    return(.ret)
+}
+
+
+
+## TRANSFORM Training Set 
+pclass_map <- scale_map(train$Pclass, train$Survived)
+gender_map <- scale_map(train$Sex, train$Survived)
+embark_map <- scale_map(train$Embarked, train$Survived)
+
+train$embark_alt <- embark_map[train$Embarked]
+train$embark_alt[train$Embarked=='' | is.na(train$Embarked)] <- mean(train$embark_alt, na.rm=TRUE)
+
+train$under10 <- 0
+train$under10[train$Age <10] <- 1
+
+age_map <- scale_map(train$under10, train$Survived)
+
+train_alt <- data.frame(
+    survived = train$Survived,
+    pid = train$PassengerId,
+    pclass = pclass_map[train$Pclass],
+    gender = gender_map[train$Sex],
+    embark = train$embark_alt,
+    under10 = age_map[as.character(train$under10)])
+
+test$embark_alt <- embark_map[test$Embarked]
+test$embark_alt[test$Embarked=='' | is.na(test$Embarked)] <- mean(train$embark_alt, na.rm=TRUE)
+
+test$under10 <- 0
+test$under10[test$Age <10] <- 1
+
+test_alt <- data.frame( pid = test$PassengerId,
+    pclass = pclass_map[test$Pclass],
+    gender = gender_map[test$Sex],
+    embark = test$embark_alt,
+    under10 = age_map[as.character(test$under10)])
+    
+    
+for(i in 1:12){
+    test_index <- sample(seq_along(train_alt$survived), 90, replace=FALSE)
+    chunk1 <- mydata[-test_index,]
+    chunk2 <- mydata[test_index,]
+    knn(chunk1[,3:6], chunk2[,2:5], train_alt$survived, k=i)
+}
+
+
+
+test_knn <- function(x, i){
+    test_index <- sample(seq_along(x$survived), 80, replace=FALSE)
+    chunk1 <- x[-test_index,]
+    chunk2 <- x[test_index,]
+    chunk2$prediction <- knn(chunk1[,3:6], chunk2[,3:6], chunk1$survived, k=i)
+    return(sum(chunk2$survived == chunk2$prediction)/80)
+}
+
+objective_k <- lapply(1:20, function(i){
+    .ret <- replicate(100, test_knn(train_alt, i))
+    return(data.frame(k=i, avg_score=mean(.ret)))
+})
+
+objective_k <- do.call("rbind", objective_k)
+plot(objective_k$k, objective_k$avg_score)
+
+### Empirically k = 6
+
+test_alt$prediction <- knn(train_alt[,3:6], test_alt[,2:5], train_alt$survived, k=6)
+
+output <- paste(test_alt$pid, test_alt$prediction, sep=",")
+output <- paste(output, collapse="\n")
+sink("output.csv")
+cat("PassengerId,Survived", fill=TRUE)
+cat(output, fill=TRUE)
+sink()
+
+
