443ec2057038f16c7abcd2703fa0f90905738da3
Tommy Shen <Gimperion@gmail.com>
2014-03-25 20:56:19 -0400

    add over 60 variable

--- titanic_read.R
+++ titanic_read.R
@@ -5,14 +5,12 @@ source("../auto/tomkit.R")
 test <- read.csv("./data/test.csv")
 train <- read.csv("./data/train.csv")
 
-
 scale_map <- function(x,y){
     a <- aggregate(y ~ x, data=data.frame(x,y), mean)
     .ret <- a$y/min(a$y)
     names(.ret) <- a$x
     return(.ret)
 }
-
 
 
 ## TRANSFORM Training Set 
@@ -26,7 +24,11 @@ train$embark_alt[train$Embarked=='' | is.na(train$Embarked)] <- mean(train$embar
 train$under10 <- 0
 train$under10[train$Age <10] <- 1
 
-age_map <- scale_map(train$under10, train$Survived)
+train$over60 <- 0
+train$over60[train$Age >60] <- 1
+
+age_map1 <- scale_map(train$under10, train$Survived)
+age_map2 <- scale_map(train$over60, train$Survived)
 
 train_alt <- data.frame(
     survived = train$Survived,
@@ -34,7 +36,8 @@ train_alt <- data.frame(
     pclass = pclass_map[train$Pclass],
     gender = gender_map[train$Sex],
     embark = train$embark_alt,
-    under10 = age_map[as.character(train$under10)])
+    under10 = age_map1[as.character(train$under10)],
+    over60 = age_map2[as.character(train$over60)])
 
 test$embark_alt <- embark_map[test$Embarked]
 test$embark_alt[test$Embarked=='' | is.na(test$Embarked)] <- mean(train$embark_alt, na.rm=TRUE)
@@ -42,27 +45,22 @@ test$embark_alt[test$Embarked=='' | is.na(test$Embarked)] <- mean(train$embark_a
 test$under10 <- 0
 test$under10[test$Age <10] <- 1
 
+test$over60 <- 0
+test$over60[test$Age >60] <- 1
+
 test_alt <- data.frame( pid = test$PassengerId,
     pclass = pclass_map[test$Pclass],
     gender = gender_map[test$Sex],
     embark = test$embark_alt,
-    under10 = age_map[as.character(test$under10)])
+    under10 = age_map[as.character(test$under10)],
+    over50 = age_map2[as.character(test$over60)])
     
     
-for(i in 1:12){
-    test_index <- sample(seq_along(train_alt$survived), 90, replace=FALSE)
-    chunk1 <- mydata[-test_index,]
-    chunk2 <- mydata[test_index,]
-    knn(chunk1[,3:6], chunk2[,2:5], train_alt$survived, k=i)
-}
-
-
-
 test_knn <- function(x, i){
     test_index <- sample(seq_along(x$survived), 80, replace=FALSE)
     chunk1 <- x[-test_index,]
     chunk2 <- x[test_index,]
-    chunk2$prediction <- knn(chunk1[,3:6], chunk2[,3:6], chunk1$survived, k=i)
+    chunk2$prediction <- knn(chunk1[,3:7], chunk2[,3:7], chunk1$survived, k=i)
     return(sum(chunk2$survived == chunk2$prediction)/80)
 }
 
@@ -74,9 +72,9 @@ objective_k <- lapply(1:20, function(i){
 objective_k <- do.call("rbind", objective_k)
 plot(objective_k$k, objective_k$avg_score)
 
-### Empirically k = 6
+### Empirically k = 4
 
-test_alt$prediction <- knn(train_alt[,3:6], test_alt[,2:5], train_alt$survived, k=6)
+test_alt$prediction <- knn(train_alt[,3:7], test_alt[,2:6], train_alt$survived, k=4)
 
 output <- paste(test_alt$pid, test_alt$prediction, sep=",")
 output <- paste(output, collapse="\n")
